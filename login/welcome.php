a15e2322a8677559fa11f13836e7d0a892728d94
Martin Didriksen <martindidriksen@hotmail.com>
2018-02-15 19:44:04 +0100

    Oprettelse af et login/registrerings system med MySQL database!

---
+++ login/welcome.php
@@ -0,0 +1,28 @@
+<?php
+// Initialize the session
+session_start();
+
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: login.php");
+  exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" type="text/css" href="css/css.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hej, <b><?php echo $_SESSION['username']; ?></b>. Velkommen til Broomie!</h1>
+    </div>
+    <p><a href="logout.php" class="btn btn-danger">Log ud</a></p>
+</body>
+</html>
