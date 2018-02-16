02f9725006a1781f62632c751c937cd4fb42c21a
Martin Didriksen <martindidriksen@hotmail.com>
2018-02-16 23:10:34 +0100

    added dashboard stuff

--- index.php
+++ index.php
@@ -24,8 +24,24 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
     </div>
 
 
+  <div class="dashwrapper">
 
+    <div class="row">
+      <div class="column" style="background-color:#b3caef;">
+        <h2>Navigation</h2>
+        <p>Some text..</p>
+      </div>
+      <div class="column" style="background-color:#bbb;">
+        <h2>Indmeldt</h2>
+        <p>Some text..</p>
+      </div>
+      <div class="column" style="background-color:#ccc;">
+        <h2>Detajler</h2>
+        <p>Some text..</p>
+      </div>
+    </div>
 
+  </div>
 
 
     <div id="logout-button">
