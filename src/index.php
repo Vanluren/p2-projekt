ec3847905a7061d1cb57e7c5b2003a8eba8e41eb
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-02-22 12:14:48 +0100

    updated directory structure to match that of the new buildsystem

--- src/index.php
+++ src/index.php
@@ -1,8 +1,46 @@
 <?php
-	include 'header.php'
+
+	// Initialize the session
+	session_start();
+
+	// If session variable is not set it will redirect to login page
+	if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+	    header("Location: login.php");
+	    exit;
+	}
+
+include 'modules/header.php'
 ?>
 
-	<h1 class='hello-world'>HELLO WORLD</h1>
+	<div class="page-header">
+
+		<h1>Hej, <b><?php echo $_SESSION['username']; ?></b>. Velkommen til Broomie!</h1>
+	</div>
+
+
+	<div class="dashwrapper">
+
+		<div class="row">
+			<div class="column" style="background-color:#b3caef;">
+				<h2>Navigation</h2>
+				<p>Some text..</p>
+			</div>
+			<div class="column" style="background-color:#bbb;">
+				<h2>Indmeldt</h2>
+				<p>Some text..</p>
+			</div>
+			<div class="column" style="background-color:#ccc;">
+				<h2>Detajler</h2>
+				<p>Some text..</p>
+			</div>
+		</div>
+
+	</div>
+
+
+	<div id="logout-button">
+		<p><a href="logout.php" class="btn btn-danger">Log ud</a></p>
+	</div>
 
 <?php
 include 'footer.php'
