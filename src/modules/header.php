f92eddaf3547081927ac5cdd8d015a6bd0c8c9e6
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-04-19 10:56:22 +0200

    færdiggjorde header, lavet lidt om i css'en, så det fungerer.

--- src/modules/header.php
+++ src/modules/header.php
@@ -5,26 +5,29 @@
     <meta name='viewport'
           content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-    <link rel='stylesheet' href='public\styles\header.css'>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel='stylesheet' href='public/styles/app.css'>
+      <link rel='stylesheet' href='public/styles/header.css'>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 	<title>Nørresundby boligselskab</title>
 </head>
 
 <body>
-  <div class="container-fluid header__container ">
-
-    <div class="header-left__container">
-        <a href="index.php">
-        <img src="../src/assets/images/nsb_logo.png">
-        </a>
-    </div>
-
-    <div class="header-mid__container">
-      <h2>Velkommen <b><?php echo $_SESSION['username']; ?></h2>
-    </div>
-
-    <div class="header-right__container">
-      <?php include 'src/modules/right_head.php' ?>
-    </div>
-
+  <div class="container-fluid header__container">
+      <div class="col-sm-4">
+        <div class="header-left__container">
+            <a href="index.php">
+              <img src="src/assets/images/nsb_logo.png" class="header-left_image">
+            </a>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="header-mid_container">
+          <h2 class="header-mid__text">Velkommen <span class="header-mid__text--bold"><?php echo $_SESSION['username']; ?></span></h2>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="header-right__container">
+          <?php include 'src/modules/vice_header.php' ?>
+        </div>
+      </div>
   </div>
