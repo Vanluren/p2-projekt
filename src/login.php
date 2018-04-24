4fc9aa46635ade87ee692372e54a0b921b526b4f
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-04-24 13:44:48 +0200

    fixed mysql config

--- src/login.php
+++ src/login.php
@@ -26,7 +26,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT username, password FROM users WHERE username = ?";
+        $sql = "SELECT username, password FROM loginb WHERE username = ?";
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
