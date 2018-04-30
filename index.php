49c2d4e94ae09d4ac8639cdf5b1c577d8fada5fa
Morild <anthon.morild@hotmail.com>
2018-04-30 13:40:35 +0200

    lidt lækkert med stier

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
     // Initialize the session
     session_start();
 
--- src/modules/header.php
+++ src/modules/header.php
@@ -1,3 +1,28 @@
+<?php
+//Vi finder request url'en
+$REQ_URI = $_SERVER['REQUEST_URI'];
+//deler hvert led ind i et array
+$REQ_URI_EXPL = explode('/', $REQ_URI);
+
+//Hvis src findes i array'et, så ved vi at pathen er til en subdir
+if(in_array('src',$REQ_URI_EXPL)){
+    //Så finder vi hvor fra vi skal fjerne alle subir paths
+    $src_index = array_search('src', $REQ_URI_EXPL);
+    //så slicer du fra 0 til første subdir path
+    $ROOT = array_slice($REQ_URI_EXPL,0, $src_index);
+    //vi bygger stien igen
+    $ROOT_DIR_PATH = implode('/',$ROOT);
+
+    //og sætter globale variabler
+    define('ROOT_PATH',$ROOT_DIR_PATH);
+    define('CSS_PATH',$ROOT_DIR_PATH.'/public/styles/app.css');
+}else{
+    //hvis ikke src findes i arrayet, ved vi at vi er i root-folderen
+    //så vi sætter globale variabler til den nuværende sti.
+    define('ROOT_PATH',$REQ_URI);
+    define('CSS_PATH',$REQ_URI.'/public/styles/app.css');
+}
+?>
 <!doctype html>
 <html lang='en'>
 <head>
@@ -5,7 +30,7 @@
     <meta name='viewport'
           content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-      <link rel="stylesheet" href="public/styles/app.css">
+      <link rel="stylesheet" href="<?php echo CSS_PATH; ?>">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Nørresundby boligselskab</title>
@@ -18,7 +43,7 @@
       <div class="col-sm-4">
         <div class="header-left__container">
             <a href="index.php">
-              <img src="src/assets/images/nsb_logo.png" class="header-left__image">
+              <img src="<?php echo ROOT_PATH ?>/src/assets/images/nsb_logo.png" class="header-left__image">
             </a>
         </div>
       </div>
