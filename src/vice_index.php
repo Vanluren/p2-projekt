b1388ba3b888872605f6a3a04e309543337b4698
lsvenn <lsvenn16@studant.aau.dk>
2018-05-01 10:12:51 +0200

    Hej Anthon. Jeg har lavet en lille sorteringsmulighed for viceværterne, så de kan sorterer deres ticket efter nyeste og prioritet.

--- src/vice_index.php
+++ src/vice_index.php
@@ -1,14 +1,15 @@
 <?php
 
+$ordervalue = 'created_at';
 $curr_department = $_SESSION['department'];
-$sql_stmt = "SELECT * FROM ticket WHERE department = $curr_department";
+$sql_stmt = "SELECT * FROM ticket WHERE department = $curr_department ORDER BY $ordervalue";
 $lejlighed_num = 0;
 
 include_once 'config.php';
 
     $result = mysqli_query($link, $sql_stmt);
-
  ?>
+
 
 
 <div class="container-fluid"><!-- <div> Index container-fluid åben -->
@@ -18,9 +19,27 @@ include_once 'config.php';
 	<!-- Sidebar luk -->
 	<div class="col-sm offset-sm-1">
 
+    <form action="" method="post">
+    <input type="radio" name="radio" value="created_at">Nyeste
+    <input type="radio" name="radio" value="priority">Prioritet
+    <input type="submit" name="submit" value="Sorter efter" />
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+      if(isset($_POST['radio']))
+      {
+        $ordervalue = $_POST['radio'];
+        $sql_stmt = "SELECT * FROM ticket WHERE department = $curr_department ORDER BY $ordervalue";
+        $result = mysqli_query($link, $sql_stmt);
+      }
+    }
+?>
+
     <?php
       if(mysqli_num_rows($result) >0):
         while ($row = mysqli_fetch_assoc($result)):
+
      ?>
      <a href="src/ticket_view.php?ticket=<?php echo $row['ticket_id']; ?>">
        <div class="row">
