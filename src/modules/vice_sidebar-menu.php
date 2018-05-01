84a9cde63c7e980dfb7025b87f697c4d4d6097b6
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-05-01 19:28:34 +0200

    ændrede struktur og styling af sidebar, så denne følger bootrap 4

--- src/modules/vice_sidebar-menu.php
+++ src/modules/vice_sidebar-menu.php
@@ -1,42 +1,27 @@
-<aside class="vice-sidebar__wrapper">
-  <div class="vice-sidebar">
-      <div class="vice-sidebar__item">
-        <a href="index.php" class="vice-sidebar__link">
-          <div class="vice-sidebar__element">Hjem</div>
-        </a>
-      </div>
-      <div class="vice-sidebar__item">
-        <a href="" class="vice-sidebar__link">
-          <div class="vice-sidebar__element">
-            Skader
-          </div>
-        </a>
-      </div>
-      <div class="vice-sidebar__item">
-        <a href="" class="vice-sidebar__link">
-          <div class="vice-sidebar__element">
-            Klager
-          </div>
-        </a>
-      </div>
-      <div class="vice-sidebar__item">
-        <a href="" class="vice-sidebar__link">
-          <div class="vice-sidebar__element">
-            Historik
-          </div>
-        </a>
-      </div>
-      <div class="vice-sidebar__item">
-        <a href="" class="vice-sidebar__link">
-          <div class="vice-sidebar__element">
-            FAQ
-          </div>
-        </a>
-      </div>
-      <div class="vice-sidebar__item">
-        <div class="vice-sidebar__link">
-          <a href="./src/logout.php" class="btn btn-danger">Log ud</a>
-        </div>
-      </div>
-    </div>
-  </aside>
+<aside class='col-md-2 vice-sidebar__wrapper'>
+	<nav class='sidebar-sticky'>
+		<div class='nav-item vice-sidebar__item active'>
+			<a href="./" class="vice-sidebar__link">
+					Skader
+			</a>
+		</div>
+		<div class='nav-item vice-sidebar__item'>
+			<a href="" class="vice-sidebar__link">
+				Klager
+			</a>
+		</div>
+		<div class='nav-item vice-sidebar__item'>
+			<a href="" class="vice-sidebar__link">
+				Historik
+			</a>
+		</div>
+		<div class='nav-item vice-sidebar__item'>
+			<a href="" class="vice-sidebar__link">
+				FAQ
+			</a>
+		</div>
+		<div class='nav-item vice-sidebar__item'>
+			<a href="./src/logout.php" class="btn btn-danger">Log ud</a>
+		</div>
+	</nav>
+</aside>
