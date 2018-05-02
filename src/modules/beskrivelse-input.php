975ea5ed098e1f2eb6faa8308db2ac2a3ab86d1d
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-05-02 20:10:49 +0200

    nææææsten, helt færdig med styling og struktur

--- src/modules/beskrivelse-input.php
+++ src/modules/beskrivelse-input.php
@@ -2,7 +2,7 @@
   <div class="col-sm-12">
     <div class="beskrivelse__wrapper">
       <h2 class="skade__input-header">Beskrivelse <i class="fa fa-question-circle"></i></h2>
-      <textarea class="beskrivelse__text-area" name="beskrivelse" rows="8" cols="50" placeholder="Beskriv skaden..."></textarea>
+      <textarea class="beskrivelse__text-area" name="beskrivelse" rows="8" cols="55" placeholder="Beskriv skaden..."></textarea>
     </div>
   </div>
 </div>
--- src/modules/billede-upload.php
+++ src/modules/billede-upload.php
@@ -1,5 +1,8 @@
-<div class="row">
+<div class="row skade__input-group">
   <div class="col-sm-12">
-    <div class="image-upload__wrapper"><button class="image-upload__btn">Vedhæft Billede</button></div>
+    <div class="image-upload__wrapper">
+	    <span>Vedhæft billede</span>
+	    <button class="image-upload__btn">Vedhæft Billede</button>
+    </div>
   </div>
 </div>
