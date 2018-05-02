65e4aee7eb001665a0789be0598e869566b18eb6
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-05-02 19:49:41 +0200

    Tilføjede en modal til ikke fungerende menu-punkter, centrerede log ud knap

--- src/beboer_index.php
+++ src/beboer_index.php
@@ -1,9 +1,6 @@
 <div class="container-fluid"><!-- <div> Index container-fluid åben -->
   <div class="container">
-
       <?php include 'modules/beboer_menu.php'; ?>
-
-      <a href="./src/logout.php" class="btn btn-danger">Log ud</a>
   </div>
 	<?php include_once 'modules/footer.php'?>
 </div><!-- Index container-fluid luk -->
--- src/modules/beboer_menu.php
+++ src/modules/beboer_menu.php
@@ -21,7 +21,7 @@
       </div>
       <div class="row">
         <div class="col-sm-12">
-          <a href="#" class="beboer-menu__link">
+          <a href="#" class="beboer-menu__link" data-toggle="modal" data-target="#avavavModal">
             <div class="beboer-menu__item">
               <h2>Status på henvendelse</h2>
             </div>
@@ -30,13 +30,34 @@
       </div>
       <div class="row">
         <div class="col-sm-12">
-          <a href="#" class="beboer-menu__link">
+          <a href="#" class="beboer-menu__link" data-toggle="modal" data-target="#avavavModal">
             <div class="beboer-menu__item">
               <h2>FAQ</h2>
             </div>
           </a>
         </div>
       </div>
+	    <div class="row">
+		    <div class="col-sm-12">
+			    <a href="<?php echo ROOT_PATH?>/src/logout.php" class="btn btn-danger">
+				    Log ud
+			    </a>
+		    </div>
+	    </div>
     </div>
   </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade ticket-modal" id="avavavModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				Der er desværre ingenting at se her.
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-primary" data-dismiss="modal">Say no more, fam..</button>
+			</div>
+		</div>
+	</div>
 </div>
