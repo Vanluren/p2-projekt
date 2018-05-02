5fcf3b71e83e49eb949b687f0d29359111ea873a
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-05-02 19:50:35 +0200

    lidt refactor af indentation og whitespace

--- src/anmeld_skade.php
+++ src/anmeld_skade.php
@@ -1,18 +1,18 @@
 <?php
-    // Include config file
-	require_once '../config/config.php';
+// Include config file
+require_once '../config/config.php';
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
-      $mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    $mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-      /* check connection */
-        if (!$mysqli) {
-          printf("Connect failed: %s\n", mysqli_connect_error());
-          exit();
-        }
-        $sql_stmt = 'INSERT INTO ticket( beskrivelse_text,
+    /* check connection */
+    if (!$mysqli) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+    $sql_stmt = 'INSERT INTO ticket( beskrivelse_text,
                                          name,
                                          priority,
                                          location,
@@ -22,65 +22,63 @@
                                          department,
                                          creater_id,
                                          address) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-                                         
-        $stmt = mysqli_prepare($link, $sql_stmt);
 
-        $beskrivelse_string = $_POST['beskrivelse'];
-        $lokalisering_string = $_POST['lokalisering'];
-        $prioritering_int = $_POST['prioritering'];
-        $navn_string = $_POST['navn'];
-        $tlf_int = $_POST['telefon'];
-        $email_string = $_POST['email'];
-        $image_path_string="";
-        $department_int = 00;
-        $beboer_id_int=1;
-        $addresse_string = "";
-        // $user_type = $_SESSION['user-type'];
+    $stmt = mysqli_prepare($link, $sql_stmt);
 
-        mysqli_stmt_bind_param( $stmt,
-                                "sssssisiis",
-                                $beskrivelse_string,
-                                $navn_string,
-                                $prioritering_int,
-                                $lokalisering_string,
-                                $email_string,
-                                $tlf_int,
-                                $image_path_string,
-                                $department_int,
-                                $beboer_id_int,
-                                $addresse_string);
+    $beskrivelse_string = $_POST['beskrivelse'];
+    $lokalisering_string = $_POST['lokalisering'];
+    $prioritering_int = $_POST['prioritering'];
+    $navn_string = $_POST['navn'];
+    $tlf_int = $_POST['telefon'];
+    $email_string = $_POST['email'];
+    $image_path_string = "";
+    $department_int = 00;
+    $beboer_id_int = 1;
+    $addresse_string = "";
+    // $user_type = $_SESSION['user-type'];
 
-
+    mysqli_stmt_bind_param($stmt,
+        "sssssisiis",
+        $beskrivelse_string,
+        $navn_string,
+        $prioritering_int,
+        $lokalisering_string,
+        $email_string,
+        $tlf_int,
+        $image_path_string,
+        $department_int,
+        $beboer_id_int,
+        $addresse_string);
 
 
-        mysqli_stmt_execute($stmt);
-          /* close connection */
-        mysqli_close($link);
-    }
-  include 'modules/header.php';
+    mysqli_stmt_execute($stmt);
+    /* close connection */
+    mysqli_close($link);
+}
+include 'modules/header.php';
 ?>
 
 <div class="container">
-  <div class="row">
-    <div class="col-10 offset-1">
-      <form  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="skade__form-wrapper">
-        <?php
-          include 'modules/lokalisering-input.php';
+	<div class="row">
+		<div class="col-10 offset-1">
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"
+			      class="skade__form-wrapper">
+                <?php
+                include 'modules/lokalisering-input.php';
 
-          include 'modules/beskrivelse-input.php';
+                include 'modules/beskrivelse-input.php';
 
-          include 'modules/billede-upload.php';
+                include 'modules/billede-upload.php';
 
-          include 'modules/prioritering-input.php';
+                include 'modules/prioritering-input.php';
 
-          include 'modules/kontakt-oplysninger-input.php'
-        ?>
-	      <div class='row skade__input-group'>
-		      <input type="submit" value="Send andmeldelse">
-	      </div>
-      </form>
-    </div>
-  </div>
+                include 'modules/kontakt-oplysninger-input.php'
+                ?>
+				<div class='row skade__input-group'>
+					<button type="submit" class='btn btn-primary'>Send anmeldelse</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </div>
-
- <?php include 'modules/footer.php'; ?>
+<?php include 'modules/footer.php'; ?>
