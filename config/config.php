d0ef2df966af28b0f5e46f3200daaec15c5d1790
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-05-04 13:29:30 +0200

    igen

--- config/config.php
+++ config/config.php
@@ -4,8 +4,8 @@ require_once 'database.php';
 /**
  * MULIGHED FOR BEDRE FEJLBESKEDE
  */
-/*ini_set('display_errors', 'On');
-error_reporting(E_ALL | E_STRICT);*/
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
 
 session_start();
 if(!isset($_SESSION['username']) && empty($_SESSION['username'])){
